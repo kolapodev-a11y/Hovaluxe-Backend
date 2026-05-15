66ed2ec1e582936d8ab5dbf5130b42f81c72a8a4
kolapodev-a11y <kolapodev@gmail.com>
2026-05-15 10:25:48 +0100

    Add files via upload

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const publicController = require('../controller/publicController');
 const { asyncHandler } = require('../utils/http');
+const { requireAuth } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/health', publicController.health);
 router.get('/config/public', publicController.getPublicConfig);
 router.get('/products', publicController.getProducts);
-router.post('/payments/flutterwave/checkout', publicController.createFlutterwaveCheckout);
+router.post('/payments/flutterwave/checkout', requireAuth, publicController.createFlutterwaveCheckout);
 router.get('/payments/flutterwave/verify', publicController.verifyFlutterwavePayment);
 router.post('/webhooks/flutterwave', asyncHandler(publicController.handleFlutterwaveWebhook));
 
